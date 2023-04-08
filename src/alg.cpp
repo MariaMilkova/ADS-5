698d9aacb3e049d6390672f5ed4d9d0706068e99
MariaMilkova <116704409+MariaMilkova@users.noreply.github.com>
2023-04-08 22:41:09 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -17,11 +17,13 @@ int getPrior(char op) {
         return 3;
     case '/':
         return 3;
+    default:
+        return 0;
     }
 }
 
 std::string infx2pstfx(std::string inf) {
-    Tstack<char, 100> stack1;
+    TStack<char, 100> stack1;
     std::string result = "";
     for (int i = 0; i < inf.size(); i++) {
         if (isdigit(inf[i]) != 0) {
@@ -31,26 +33,26 @@ std::string infx2pstfx(std::string inf) {
                 result += " ";
                 result += inf[i];
             }
-        } else if (getPrior(inf[i]) == 0 || getPrior(inf[i]) > getPrior(stack1.get()) || stack1.isEmpty()) {
-            if (getPrior(inf[i]) == 0 && stack1.isEmpty() || !isdigit(inf[i - 1])) {
+        } else if (getPrior(inf[i]) == 0 ||
+            getPrior(inf[i]) > getPrior(stack1.get()) ||stack1.isEmpty()) {
+            if (getPrior(inf[i]) == 0 && stack1.isEmpty() ||
+                !isdigit(inf[i - 1])) {
                 stack1.push(inf[i]);
             } else {
-                //result += " ";
                 stack1.push(inf[i]);
             }
         } else if (getPrior(inf[i]) == 2 || getPrior(inf[i]) == 3) {
             while (getPrior(inf[i]) <= getPrior(stack1.get())) {
-                
+                result += " ";
                 result += stack1.pop();
-                //result += " ";
             }
             stack1.push(inf[i]);
         } else if (getPrior(inf[i]) == 1) {
             while (getPrior(stack1.get()) != 0) {
                 result += " ";
                 result += stack1.pop();
-            } if (isdigit(inf[i + 1]) || getPrior(inf[i + 1]) == 1) {
-                //result += " ";
+            }
+            if (isdigit(inf[i + 1]) || getPrior(inf[i + 1]) == 1) {
                 stack1.pop();
             } else {
                 stack1.pop();
@@ -65,7 +67,7 @@ std::string infx2pstfx(std::string inf) {
 }
 
 int eval(std::string post) {
-    Tstack<int, 100> stack2;
+    TStack<int, 100> stack2;
     for (int i = 0; i < post.size(); i++) {
         if (isdigit(post[i])) {
             int value = post[i] - '0';
